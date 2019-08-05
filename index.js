da3cf8e8890be23e3d75c8256812c2bb8bde221b
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-05 12:19:46 -0400

    Done.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-animal = ""
+let animal = "cat"
 
 function myAnimal() {
+  let animal = "dog"
   return animal
 }
 
@@ -12,8 +13,8 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  let two = 2
   return n + two
 
   // Feel free to move things around!
-  two = 2
 }
